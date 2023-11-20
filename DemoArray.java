612676b7d4e1f72565f91bb05bb2e70721525a87
Ramesh7676108425 <rameshl.xworkz@gmail.com>
2023-11-20 13:37:51 +0530

    arryas

---
+++ DemoArray.java
@@ -0,0 +1,24 @@
+package com.things;
+
+import java.util.Arrays;
+
+public class DemoArray {
+	public static void main(String[] args) {
+		int[] a= {4,3,1,2};
+		
+		for(int i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				
+				if(a[i]>a[j]) {
+					
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+				System.out.println(Arrays.toString(a));
+			}
+		}
+		
+	}
+
+}
